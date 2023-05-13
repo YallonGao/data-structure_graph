9b67390774235c5b295db21255487bcc33151903
YallonGao <2975930792@qq.com>
2023-05-13 23:45:54 +0800

    Hello, World!

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World! " << endl;
-    cout << "Hello, Graph! " << endl;
+    cout << "Hello, World!" << endl;
+    cout << "Hello, Graph!" << endl;
     return 0;
 }
